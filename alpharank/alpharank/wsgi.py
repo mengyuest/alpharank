d8bb755bf829b71df5bcdfb4d6112e7107f67705
Yue Meng <mengyuethu@gmail.com>
2018-02-15 11:57:32 -0800

    alpharank

--- alpharank/alpharank/wsgi.py
+++ alpharank/alpharank/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for alpha project.
+WSGI config for alpharank project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alpha.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alpharank.settings")
 
 application = get_wsgi_application()
